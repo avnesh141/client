c1796ad951322623cf177e2b6c4b87c428d9722f
soojal0903 <soojal.singh.ece21@itbhu.ac.in>
2023-03-25 23:05:17 +0530

    changes

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './header'
+
+function App() {
+  return (
+    <div>
+      {/* <h1>GROW</h1> */}
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
