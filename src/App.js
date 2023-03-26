518b4ea30d65891d40ccbb62f4c237c0c19f5e64
soojal0903 <soojal.singh.ece21@itbhu.ac.in>
2023-03-27 00:18:49 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import Login from './components/Login_page/Login_page'
-import Signup from'./components/Signup/Signup'
+// import Login from './components/Login_page/Login_page'
+// import Signup from'./components/Signup/Signup'
 const App = () => {
   return (
     <div>
