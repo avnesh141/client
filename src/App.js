bf96214b5098e7c101e8936a061a8a4d4df3c1f4
soojal0903 <soojal.singh.ece21@itbhu.ac.in>
2023-03-27 23:42:01 +0530

    login page,signup page,footer done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import Header from './components/Header/Header'
+// import Header from './components/Header/Header'
 // import Login from './components/Login_page/Login_page'
 // import Signup from'./components/Signup/Signup'
+// import Aboutus from './components/Aboutus/AboutUs'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <div>
-      <Header/>
+      {/* <Header /> */}
       {/* <Login/> */}
       {/* <Signup/> */}
-
+      {/* <Aboutus/> */}
+      <Footer/>
     </div>
   )
 }
