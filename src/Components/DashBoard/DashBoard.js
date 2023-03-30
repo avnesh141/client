081d7f6392ea1d57117996ddf4dd1fd5ac94270f
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2023-03-30 21:09:50 +0530

    dashboard

--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,152 +1,45 @@
-import React from 'react'
-import "./Dashboard.css"
+import React, { useEffect, useState } from "react";
+import ComapanyCard from "./ComapanyCard";
+import "./Dashboard.css";
 function Dashboard() {
-  const openbuy = () => {
-    if (document.getElementsByClassName("qtyifClickedbuy")[0].style.display === 'inline-block') {
-      document.getElementsByClassName("qtyifClickedbuy")[0].style.display = 'none'
-    }
-    else {
-      document.getElementsByClassName("qtyifClickedbuy")[0].style.display = 'inline-block';
-      document.getElementsByClassName("qtyifClickedsell")[0].style.display = 'none';
-    }
-  }
-  const opensell = () => {
-    if (document.getElementsByClassName("qtyifClickedsell")[0].style.display === 'inline-block') {
-      document.getElementsByClassName("qtyifClickedsell")[0].style.display = 'none'
-    }
-    else {
-      document.getElementsByClassName("qtyifClickedsell")[0].style.display = 'inline-block';
-      document.getElementsByClassName("qtyifClickedbuy")[0].style.display = 'none';
-    }
-  }
+  const [bought, setbought] = useState([]);
+  const boughtfunc = async () => {
+    const response = await fetch(`http://localhost:5000/api/invest/get`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authtoken: JSON.stringify(localStorage.getItem("token")),
+      },
+    });
+    let parsedata = await response.json();
+    let finaldata = Object.values(parsedata.stocks);
+    setbought(finaldata);
+    console.log(finaldata);
+    console.log(typeof bought);
+  };
+
+  useEffect(() => {
+    boughtfunc();
+  },[]);
+
   return (
-    <div className='Dashboard-page'>
-      <div className='topportContainer'>
-        <div className='portList'>
-          <div className='portCard'>
-            <div className='portContainer'>
-              <div className="comName">
-                <h5>Adani Green</h5>
-                <h6>NET QTY 1</h6>
-              </div>
-              <div className="ltp">
-                <h6>LTP ₹37.10</h6>
-                <h6>P&L ₹13.20</h6>
-              </div>
-              <div className="buy" onClick={(e) => {
-                e.preventDefault();
-                openbuy();
-              }}>
-                Buy
-              </div>
-              <div className="sell" onClick={(e) => {
-                e.preventDefault();
-                opensell();
-              }}>
-                Sell
-              </div>
-            </div>
-            <div className="qtyifClickedbuy">
-              <p>Amount you want to buy:
-                <input type="number" />
-              </p>
-              <p>
-                Net value = Qty X LTP
-              </p>
-              <button>Confirm trans.</button>
-            </div>
-            <div className="qtyifClickedsell">
-              <p>Amount you want to sell:
-                <input type="number" />
-              </p>
-              <p>
-                Net value = Qty X LTP
-              </p>
-              <button>Confirm trans.</button>
-            </div>
-          </div>
-        </div>
-        <div className='portList'>
-          <div className='portCard'>
-            <div className='portContainer'>
-              <div className="comName">
-                <h5>Adani Green</h5>
-                <h6>NET QTY 1</h6>
-              </div>
-              <div className="ltp">
-                <h6>LTP ₹37.10</h6>
-                <h6>P&L ₹13.20</h6>
-              </div>
-              <div className="buy" onClick={(e) => {
-                e.preventDefault();
-                openbuy();
-              }}>
-                Buy
-              </div>
-              <div className="sell" onClick={(e) => {
-                e.preventDefault();
-                opensell();
-              }}>
-                Sell
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='portList'>
-          <div className='portCard'>
-            <div className='portContainer'>
-              <div className="comName">
-                <h5>Adani Green</h5>
-                <h6>NET QTY 1</h6>
-              </div>
-              <div className="ltp">
-                <h6>LTP ₹37.10</h6>
-                <h6>P&L ₹13.20</h6>
-              </div>
-              <div className="buy" onClick={(e) => {
-                e.preventDefault();
-                openbuy();
-              }}>
-                Buy
-              </div>
-              <div className="sell" onClick={(e) => {
-                e.preventDefault();
-                opensell();
-              }}>
-                Sell
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='portList'>
-          <div className='portCard'>
-            <div className='portContainer'>
-              <div className="comName">
-                <h5>Adani Green</h5>
-                <h6>NET QTY 1</h6>
-              </div>
-              <div className="ltp">
-                <h6>LTP ₹37.10</h6>
-                <h6>P&L ₹13.20</h6>
-              </div>
-              <div className="buy" onClick={(e) => {
-                e.preventDefault();
-                openbuy();
-              }}>
-                Buy
-              </div>
-              <div className="sell" onClick={(e) => {
-                e.preventDefault();
-                opensell();
-              }}>
-                Sell
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="Dashboard-page">
+      <div className="topportContainer">
+        {bought.map((stock, key) => {
+          return (
+            <ComapanyCard
+              company={stock.company}
+              number={stock.number}
+              price={stock.price}
+              type={stock.type}
+              id={stock._id}
+              key={key}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
