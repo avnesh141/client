c512495117e5a4f2bbd7a8e52795fd89cd0f08aa
soojal0903 <soojal.singh.ece21@itbhu.ac.in>
2023-03-31 15:03:55 +0530

    chAGES

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Footer from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Navigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import Landing from "./Components/Landingpage/Landing"
-import { Helmet } from "react-helmet";
+// import Landing from "./Components/Landingpage/Landing"
+// import { Helmet } from "react-helmet";
 const App = () => {
   return (
       <Router>
-        <Helmet>
+        {/* <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-      </Helmet>
+      </Helmet> */}
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Landing />} />
+          {/* <Route exact path="/" element={<Landing />} /> */}
           <Route exact path="/trade" element={<Section />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
