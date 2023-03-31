46dd26309f25acbd40712a9c7c1a6e8983a8592c
soojal0903 <soojal.singh.ece21@itbhu.ac.in>
2023-03-31 13:18:50 +0530

    home

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import Footer from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Navigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Landing from "./Components/Landingpage/Landing"
+import { Helmet } from "react-helmet";
 const App = () => {
   return (
       <Router>
+        <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+      </Helmet>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Section />} />
+          <Route exact path="/" element={<Landing />} />
+          {/* <Route exact path="/" element={<Section />} /> */}
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="*" element={<Navigate to="/" />} />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "./Components/Landingpage/Landingstyle/flexboxgrid.min.css";
+import "./Components/Landingpage/Landingstyle/index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
